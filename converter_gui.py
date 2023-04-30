e882685ffe0f567cd839823970c2f43d8a3ba3f0
elif c <86270311+elif-c@users.noreply.github.com>
2023-04-30 21:03:18 +0300

    UI Changes

--- converter_gui.py
+++ converter_gui.py
@@ -3,6 +3,8 @@ import PySimpleGUI
 import converter
 from converter import convert
 
+PySimpleGUI.theme("Black")
+
 feet_text = PySimpleGUI.Text("Enter feet:")
 feet_input = PySimpleGUI.InputText(0, key="feet_input")
 
@@ -10,14 +12,15 @@ inches_text = PySimpleGUI.Text("Enter inches:")
 inches_input = PySimpleGUI.InputText(0, key="inches_input")
 
 convert_button = PySimpleGUI.Button("Convert", key="convert",
-                                    button_color="purple4")
+                                    button_color="orange")
+exit_button = PySimpleGUI.Button("Exit")
 result = PySimpleGUI.Text(key="result")
 
 window = PySimpleGUI.Window("Converter",
                             font=("Roboto", 12),
-                            layout=[[feet_text, feet_input],
+                            layout=[[feet_text, PySimpleGUI.Push(), feet_input],
                                     [inches_text, inches_input],
-                                    [convert_button, result]])
+                                    [convert_button, result, PySimpleGUI.Push(), exit_button]])
 while True:
     event_key, value = window.read()
     match event_key:
@@ -27,6 +30,8 @@ while True:
             result = converter.convert(feet, inches)
             result = round(result, 2)
             window["result"].update(result)
+        case "Exit":
+            break
         case PySimpleGUI.WINDOW_CLOSED:
             break
 window.close()
